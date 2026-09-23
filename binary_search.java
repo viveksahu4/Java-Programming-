acb68e7fd8732ab7a50a5417dd27da09d21660e3
Vivek sahu <vickysv1610@gmail.com>
2026-09-23 11:56:06 +0530

    binary search

---
+++ binary_search.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+public class binary_search {
+    static  int binarysearch(int[] arr,int target){
+        int left = 0, right = arr.length - 1;
+        while(left <= right){
+            int mid  = left + (right - left) / 2;
+
+            if(arr[mid] == target){
+                return mid;
+            }
+            else if(arr[mid] > target){
+                right = mid - 1;
+            }
+            else{
+                left = mid + 1;
+            }
+
+        }
+        return -1;
+    }
+
+    public static void main(String[] args){
+        
+        
+        int[] arr = {1,2,3,4,5,6};
+
+        System.out.print((binarysearch(arr, 5)));
+
+
+
+    }
+}
