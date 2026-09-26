21c5933c9a6fc24a57d2ea1e232c84c00444d1f9
Vivek sahu <vickysv1610@gmail.com>
2026-09-26 19:47:43 +0530

    armstrong check

---
+++ armstrong_check.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class armstrong_check {
+    public static void armcheck(int n){
+        int original = n;
+        int sum = 0;
+        while(n > 0){
+            int digit = n % 10;
+            sum = sum + (digit * digit * digit);
+            n/= 10;
+
+        }
+
+        if(sum == original) System.out.print("Armstrong");
+        else
+            System.out.print("not armstrong");
+    }
+
+
+    public static void main(String[] args){
+        Scanner sc =  new Scanner(System.in);
+        int n = sc.nextInt();
+
+        armcheck(n);
+    }
+}
